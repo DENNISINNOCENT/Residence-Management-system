2d7975dc65103e8ed326af47246d54bdfcfa00d0
DENNISINNOCENT <mutindahdennis@gmail.com>
2023-07-16 18:46:50 +0300

    path routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Home from './component/Home'
+import Rental from './component/Rental'
+import Layout from './component/Layout'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path ="/" element ={<Layout/>}/>
+      <Route index element ={<Home/>}/>
+      <Route path ="rentals" element ={<Rental/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
